f859c3a423255014cd4f013f637deae341a67a59
Peter Ingles <peter.ingles@gmail.com>
2023-07-17 14:03:38 -0500

    Added all functionality

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).ready(onReady);
+
+function onReady() {
+    console.log('in OnReady')
+
+    $('#generateButton').on('click', handleGenerate)
+    $('#generateButton').on('click', addCount)
+    $('#target').on('click', '.deleteButton', deleteThis)
+    $('#target').on('click', '.yellowButton', makeYellow)
+
+}
+
+let count = 1
+
+function handleGenerate(event){
+    event.preventDefault();
+    console.log('in handleGenerate')
+    $('body').append(`
+    <div class='redBox'>
+    <p>${count}</p>
+    <button class='yellowButton'>Yellow</button>
+    <button class='deleteButton'>Delete</button>
+    </div>`)
+}
+
+function addCount(){
+    count ++
+}
+
+function deleteThis(){
+    console.log('in deleteThis')
+    $('.redBox').remove()
+}
+
+function makeYellow(){
+    $('.redBox').addClass("yellowBox")
+}
